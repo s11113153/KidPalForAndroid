150c1c86a9f9bcd65fa9a6dfa6aca4fae19507c2
s11113153 <s11113153@stu.edu.tw>
2014-07-31 15:10:53 +0800

    add Help

--- app/src/main/java/tw/com/mobilogics/testhellohandy/MainActivity.java
+++ app/src/main/java/tw/com/mobilogics/testhellohandy/MainActivity.java
@@ -275,6 +275,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
               Toast.makeText(MainActivity.this, "help", Toast.LENGTH_LONG).show();
+              startActivity(new Intent(MainActivity.this, HelpDialog.class));
             }
           });
         }
